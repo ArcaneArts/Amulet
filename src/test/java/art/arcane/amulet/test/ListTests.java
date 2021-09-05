d00027e1ffb35913f36f2a3207cc5784aebf69d3
cyberpwn <danielmillst@gmail.com>
2021-09-04 23:29:52 -0400

    Lists tests TBC

---
+++ src/test/java/art/arcane/amulet/test/ListTests.java
@@ -0,0 +1,29 @@
+package art.arcane.amulet.test;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ListTests {
+    @Test
+    public void testCreation()
+    {
+        assertEquals(List.from("hello", "world").size(), 2);
+        assertEquals(List.from("hello", "world").getClass(), ArrayList.class);
+        assertEquals(List.from(CopyOnWriteArrayList::new, "hello", "world").getClass(), CopyOnWriteArrayList.class);
+    }
+
+    @Test
+    public void testCopy()
+    {
+        List<String> s = List.from("a", "b", "c");
+        List<String> c = s.copy().removeLast();
+        assertEquals(s.size(), 3);
+        assertEquals(c.size(), 2);
+    }
+}
