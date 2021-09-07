69a1aee76b0b0eb6b30cb75dd453834f89bd53a2
cyberpwn <danielmillst@gmail.com>
2021-09-07 05:25:32 -0400

    Future docs

--- src/main/java/Amulet/extensions/java/util/concurrent/Future/XFuture.java
+++ src/main/java/Amulet/extensions/java/util/concurrent/Future/XFuture.java
@@ -13,11 +13,20 @@ import java.util.function.Consumer;
 
 @Extension
 public class XFuture {
+  /**
+   * Force call get() and return null if anything fails without throwing exceptions
+   * @return the value or null
+   */
   public static <V> V force(@This Future<V> self)
   {
     return J.attempt(self::get);
   }
 
+  /**
+   * Wait on multiple futures of the same type while this future runs (concurrent)
+   * @param vs the futures to wait on with this future
+   * @return a future list of all the results including this future
+   */
   @SafeVarargs
   @SuppressWarnings("Convert2MethodRef")
   public static <V> Future<List<V>> and(@This Future<V> self, Future<V>... vs)
@@ -25,6 +34,14 @@ public class XFuture {
     return J.get(() -> Arrays.stream(vs).and(self).map((i) -> i.force()).toList());
   }
 
+  /**
+   * Wait on this future and multiple callables to finish in parallel,
+   * once all (including this future) have finished a list of all the values is constructed.
+   * @param self
+   * @param vs the list of callables to run concurrently
+   * @param <V>
+   * @return the future list of all the results including this future's result
+   */
   @SafeVarargs
   @SuppressWarnings("Convert2MethodRef")
   public static <V> Future<List<V>> and(@This Future<V> self, Callable<V>... vs)
@@ -32,6 +49,13 @@ public class XFuture {
     return J.get(() -> Arrays.stream(vs).map(J::get).and(self).map((i) -> i.force()).toList());
   }
 
+  /**
+   * Wait on this future and multiple runnables to complete concurrently.
+   * @param self
+   * @param vs the runnables to run in parallel
+   * @param <V>
+   * @return this future's result (after all the runnables have finished with this future concurrently)
+   */
   @SuppressWarnings("Convert2MethodRef")
   public static <V> Future<V> and(@This Future<V> self, Runnable... vs)
   {
@@ -42,6 +66,14 @@ public class XFuture {
     });
   }
 
+  /**
+   * When this future completes, run a consumer with it's result
+   * @param self
+   * @param then the consumer to call when this future completes
+   * @param <V>
+   * @return the future of the THEN being called. This future should be
+   * chained on the then consumer, not after this method if you need types.
+   */
   public static <V> Future<?> then(@This Future<V> self, Consumer<V> then)
   {
     return J.run(() -> then.accept(self.force()));
