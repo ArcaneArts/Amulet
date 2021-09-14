3340791a4c8c137c392c1dfd0a6ec2e376789375
cyberpwn <danielmillst@gmail.com>
2021-09-14 14:16:08 -0400

    Runtime access

--- src/main/java/Amulet/extensions/java/lang/Runtime/XRuntime.java
+++ src/main/java/Amulet/extensions/java/lang/Runtime/XRuntime.java
@@ -18,12 +18,116 @@
 
 package Amulet.extensions.java.lang.Runtime;
 
+import art.arcane.amulet.util.Platform;
 import manifold.ext.rt.api.Extension;
+import manifold.ext.rt.api.This;
+
+import java.io.File;
 
 @Extension
 public class XRuntime {
     @Extension
-    public static void die() {
-        System.exit(0);
+    public static Runtime get() {
+        return Runtime.getRuntime();
+    }
+
+    public static double getCPULoad(@This Runtime rt)
+    {
+        return Platform.CPU.getCPULoad();
+    }
+
+    public static long getTotalSystemMemory(@This Runtime rt)
+    {
+        return Platform.MEMORY.PHYSICAL.getTotalMemory();
+    }
+
+    public static long getUsedSystemMemory(@This Runtime rt)
+    {
+        return Platform.MEMORY.PHYSICAL.getUsedMemory();
+    }
+
+    public static long getFreeSystemMemory(@This Runtime rt)
+    {
+        return Platform.MEMORY.PHYSICAL.getUsedMemory();
+    }
+
+    public static long getTotalSystemSwap(@This Runtime rt)
+    {
+        return Platform.MEMORY.VIRTUAL.getTotalMemory();
+    }
+
+    public static long getUsedSystemSwap(@This Runtime rt)
+    {
+        return Platform.MEMORY.VIRTUAL.getUsedMemory();
+    }
+
+    public static long getFreeSystemSwap(@This Runtime rt)
+    {
+        return Platform.MEMORY.VIRTUAL.getUsedMemory();
+    }
+
+    public static long getFreeDiskSpace(@This Runtime rt)
+    {
+        return Platform.STORAGE.getFreeSpace();
+    }
+
+    public static long getFreeDiskSpace(@This Runtime rt, File root)
+    {
+        return Platform.STORAGE.getFreeSpace(root);
+    }
+
+    public static long getUsedDiskSpace(@This Runtime rt)
+    {
+        return Platform.STORAGE.getUsedSpace();
+    }
+
+    public static long getUsedDiskSpace(@This Runtime rt, File root)
+    {
+        return Platform.STORAGE.getUsedSpace(root);
+    }
+
+    public static long getTotalDiskSpace(@This Runtime rt)
+    {
+        return Platform.STORAGE.getTotalSpace();
+    }
+
+    public static long getTotalDiskSpace(@This Runtime rt, File root)
+    {
+        return Platform.STORAGE.getTotalSpace(root);
+    }
+
+    public static long getAbsoluteTotalDiskSpace(@This Runtime rt)
+    {
+        return Platform.STORAGE.getAbsoluteTotalSpace();
+    }
+
+    public static long getAbsoluteFreeDiskSpace(@This Runtime rt)
+    {
+        return Platform.STORAGE.getAbsoluteFreeSpace();
+    }
+
+    public static long getAbsoluteUsedDiskSpace(@This Runtime rt)
+    {
+        return Platform.STORAGE.getAbsoluteUsedSpace();
+    }
+
+    public static String getJavaVendor(@This Runtime rt)
+    {
+        return Platform.ENVIRONMENT.getJavaVendor();
+    }
+
+    public static String getJavaHome(@This Runtime rt)
+    {
+        return Platform.ENVIRONMENT.getJavaHome();
+    }
+
+    public static String getJavaVersion(@This Runtime rt)
+    {
+        return Platform.ENVIRONMENT.getJavaVersion();
+    }
+
+    public static double getProcessCPULoad(@This Runtime rt)
+    {
+        return Platform.CPU.getLiveProcessCPULoad();
     }
 }
