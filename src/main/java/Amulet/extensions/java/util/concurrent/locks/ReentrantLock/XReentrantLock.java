70eeb0b4d8b35d8ce25029e14eeb75060f2254a4
DanLT <danielmillst@gmail.com>
2021-09-27 09:37:37 -0800

    Reentrantlocks

--- src/main/java/Amulet/extensions/java/util/concurrent/locks/ReentrantLock/XReentrantLock.java
+++ src/main/java/Amulet/extensions/java/util/concurrent/locks/ReentrantLock/XReentrantLock.java
@@ -2,11 +2,22 @@ package Amulet.extensions.java.util.concurrent.locks.ReentrantLock;
 
 import manifold.ext.rt.api.Extension;
 import manifold.ext.rt.api.This;
+
 import java.util.concurrent.locks.ReentrantLock;
+import java.util.function.Supplier;
 
 @Extension
 public class XReentrantLock {
-  public static void helloWorld(@This ReentrantLock thiz) {
-    System.out.println("hello world!");
+  public static void run(@This ReentrantLock lock, Runnable r) {
+    lock.lock();
+    r.run();
+    lock.unlock();
+  }
+
+  public static <T> void run(@This ReentrantLock lock, Supplier<T> v)
+  {
+    lock.lock();
+    T vv = v.get();
+    lock.unlock();
   }
 }
